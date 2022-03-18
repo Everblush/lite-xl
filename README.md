59e764a82ef63324b721b7e5ab78a47b561a9f25
mangeshrex <rex835138@gmail.com>
2022-03-18 10:47:37 +0530

    Adding lite-xl port files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# lite-xl
-Port for Lite-xl Text Editor
+<h2 align="center"> Everblush Lite-XL </h2> 
+
+<p align="center">
+<img src="https://img.shields.io/static/v1?label=license&message=MIT&color=8ccf7e&labelColor=22292b&style=for-the-badge">
+</p>
+
+
+## Preview 
+<img src="./preview.png" align="center">
+
+## Installation 
+- Install [lite-xl](https://github.com/lite-xl/lite-xl).
+- Put ```colors/everblush.lua``` in ```~/.config/lite-xl/colors/```.
+- Add the following to ```$HOME/.config/lite-xl/init.lua```.
+```sh 
+core.reload_module("colors.everblush")
+```
---
+++ colors/everblush.lua
@@ -0,0 +1,29 @@
+
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#181f21" }
+style.background2 = { common.color "#22292b" }
+style.background3 = { common.color "#3b4244" }
+style.text = { common.color "#dadada" }
+style.caret = { common.color "#8ed8d1" }
+style.accent = { common.color "#67b0e8" }
+style.dim = { common.color "#bdc3c2" }
+style.divider = { common.color "#22292b" }
+style.selection = { common.color "#22292b" }
+style.line_number = { common.color "#b3b9b8" }
+style.line_number2 = { common.color "#a9afae" }
+style.line_highlight = { common.color "#22292b" }
+style.scrollbar = { common.color "#bab3e5" }
+style.scrollbar2 = { common.color "#c4bdef" }
+
+style.syntax["normal"] = { common.color "#b3b9b8" }
+style.syntax["symbol"] = { common.color "#b3b9b8" }
+style.syntax["comment"] = { common.color "#404749" }
+style.syntax["keyword"] = { common.color "#5dc1dd" }
+style.syntax["keyword2"] = { common.color "#67cbe7" }
+style.syntax["number"] = { common.color "#b3b9b8" }
+style.syntax["literal"] = { common.color "#d8b25b" }
+style.syntax["string"] = { common.color "#8ccf7e" }
+style.syntax["operator"] = { common.color "#bab3e5" }
+style.syntax["function"] = { common.color "#e06e6e" }
